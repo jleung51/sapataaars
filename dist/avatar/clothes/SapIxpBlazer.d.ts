50f67979448fbb3a93aae94e85e24b062e3962ff
Jeffrey Leung <jeffrey.leung@sap.com>
2020-07-27 00:57:21 -0700

    Remove built files

--- dist/avatar/clothes/SapIxpBlazer.d.ts
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-export default class SapIxpBlazer extends React.Component {
-    static optionValue: string;
-    render(): JSX.Element;
-}
--- dist/avatar/face/nose/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Default_1 = require("./Default");
-Object.defineProperty(exports, "default", { enumerable: true, get: function () { return Default_1.default; } });
